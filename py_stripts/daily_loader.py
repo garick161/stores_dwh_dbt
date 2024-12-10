8f0c11aebff883babedc3f9c04e528a0c8dd493a
Sergey Gagarin <s.gagarin@astondevs.ru>
2024-12-10 10:47:37 +0300

    edit function get_load_date

--- py_stripts/daily_loader.py
+++ py_stripts/daily_loader.py
@@ -15,23 +15,26 @@ SOURCE_DB_PASSWORD = config["source_db"]["password"]
 SOURCE_DB_NAME = config["source_db"]["dbname"]
 
 
-def get_last_loaded_date():
+def get_loaded_date() -> str:
+    """Функция для получения даты текущей загрузки"""
     try:
         connection = psycopg2.connect(host=SOURCE_DB_HOST, port=SOURCE_DB_PORT, user=SOURCE_DB_USER, password=SOURCE_DB_PASSWORD, dbname=SOURCE_DB_NAME)
         cursor = connection.cursor()
         cursor.execute("SELECT MAX(calday) FROM public.bills_head;")
-        res = cursor.fetchone()[0]
+        last_date = cursor.fetchone()[0]
 
     except Exception as e:
         print(f"Ошибка: {e}")
     connection.close()
-    return res
+
+    if last_date:
+        load_date = (last_date + timedelta(days=1)).strftime("%Y-%m-%d")
+    else:
+        load_date = "2021-01-01"
+
+    return load_date
 
 
 if __name__ == "__main__":
-    LOAD_DATE = get_last_loaded_date()
-    if LOAD_DATE:
-        LOAD_DATE = (LOAD_DATE + timedelta(days=1)).strftime("%Y-%m-%d")
-    else:
-        LOAD_DATE = "2021-01-01"
+    LOAD_DATE = get_loaded_date()
     print(LOAD_DATE)
