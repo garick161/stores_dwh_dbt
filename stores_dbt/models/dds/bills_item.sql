31afdae9ab3cde3e6d972af9921aaff1e71ac360
Sergey Gagarin <s.gagarin@astondevs.ru>
2024-12-15 16:23:57 +0300

    edit bills_item model

--- stores_dbt/models/dds/bills_item.sql
+++ stores_dbt/models/dds/bills_item.sql
@@ -1,3 +1,4 @@
+-- Инкрементальная загрузка дельты для таблицы bills_item в слой DDS
 {{ 
     config(
         schema='dds', 
@@ -6,7 +7,13 @@
         post_hook="{{ update_load_info_after_dds('bills_item') }}")
  }}
 
--- Инкрементальная загрузка дельты для таблицы bills_item в слой DDS
+{% if execute %}
+    {% set flag =  is_not_updated('bills_item') %}
+{% endif %}
+
+-- Если `flag` = Fasle, значит данные уже обновлены и нет необходимости загружать. 
+-- Если `flag` = True, значит данные последней дельты не загружены. Произойдет добавление новых записей в хранилище
+-- И с помощью 'post_hook' будут обновлены записи в информационной таблице `table_load_info`
 select
 	billnum::bigint,
 	billitem::bigint,
@@ -15,8 +22,8 @@ select
 	netval::numeric(17, 2),
 	tax::numeric(17, 2),
 	rpa_sat::numeric(17, 2),
-	calday 
+	calday::date
 from {{ ref('bills_item_stg') }}
 {% if is_incremental() %}
+where '{{ flag }}'
 {% endif %}
-
