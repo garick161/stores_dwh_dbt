cacf0c26a0e988fda1d10592ba1d8a2f99b8d6d3
Sergey Gagarin <s.gagarin@astondevs.ru>
2024-12-16 14:02:20 +0300

    add total_amount_count to dm

---
+++ stores_dbt/models/dm/total_amount_count.sql
@@ -0,0 +1,9 @@
+{{ config(schema='dm', materialized='view') }}
+
+select bh.plant, sum(bi.rpa_sat) as total_amount, sum(bi.qty) as total_item_count
+from {{ ref('bills_head') }} bh
+join {{ ref('bills_item') }} bi
+on bh.billnum = bi.billnum and bh.calday = bi.calday
+where bh.calday >= '{{ var('date_from') }}' and bh.calday < '{{ var('date_to') }}'
+group by bh.plant
+
